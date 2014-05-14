578bf81ae7abd06b9aa319a36f2f7d3733b70bd1
rj03hou <rj03hou@gmail.com>
2014-05-14 15:47:10 +0800

    Update chk_redis.sh

--- chk_redis.sh
+++ chk_redis.sh
@@ -8,6 +8,6 @@ log() {
 logf() {
  echo >>/var/log/redis/redis.chk.log `date  "+%F %T"` "$*"
 }
-${REDIS_BIN_DIR}/redis-cli  -h 127.0.0.1 -p ${REDIS_PORT} info >>/tmp/redis/redis.${REDIS_PORT}.info
+${REDIS_BIN_DIR}/redis-cli  -h 127.0.0.1 -p ${REDIS_PORT} info >>/tmp/redis.${REDIS_PORT}.info
 
 if [ "$?" != "0" ];then logf "redis ${REDIS_PORT} error" ;exit 1; else logf "redis ${REDIS_PORT} ok" ;exit 0 ;fi
