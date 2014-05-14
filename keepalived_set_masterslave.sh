2ecf30343275afd7134c95c16b74c060bb6aaf74
rj03hou <rj03hou@gmail.com>
2014-05-14 16:05:30 +0800

    Update keepalived_set_masterslave.sh

--- keepalived_set_masterslave.sh
+++ keepalived_set_masterslave.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#
+
+masterip=""
+
 LOCAL_IP="$(ifconfig bond0 | grep -w inet | awk '{ print $2 }' | awk -F: '{ print $2 }')"
 if [ "${LOCAL_IP}" == "localip" ]; then 
 	REDIS_MASTER_IP="masterip";
@@ -10,8 +12,8 @@ else
   exit 1
 fi
 
-REDIS_PORT="6380 6381"
-REDIS_BIN_DIR="/usr/local/redis/bin/"
+REDIS_PORT="6379"
+REDIS_BIN_DIR="/usr/bin/"
 
 chk_redis_alive () {
 	alive=$(${REDIS_BIN_DIR}/redis-cli  -h 127.0.0.1 -p ${REDIS_PORT} ping)
@@ -35,23 +37,8 @@ log() {
 start_master() {
      for port in ${REDIS_PORT};do
          cmdsetmaster="${REDIS_BIN_DIR}/redis-cli -h "127.0.0.1" -p ${port} SLAVEOF no one"
-         #${cmdsetmaster} >/dev/null ;
-        a=1
-         while [ "x${state}" != "xmaster" ];do
-             ${cmdsetmaster} >/dev/null ;
-            #${REDIS_BIN_DIR}/redis-cli -h "127.0.0.1" -p 6381 SLAVEOF  192.168.2.163  6381 >>/dev/null
-              state=$(${REDIS_BIN_DIR}/redis-cli -h 127.0.0.1 -p ${port} info|awk  -F ':' '/role/ {print $NF}'|tr -d "\r");
-              if [ ${a} -eq 5 ];then
-                /etc/init.d/redis_${port} restart >>/dev/null
-                [ $? -eq 0 ]&& msg="restart redis-server on ${port} sucessed";${cmdsetmaster} >/dev/null|| msg="tried restart redis-server on ${port} faild"
-                log "Promoting redis-server to MASTER on ${port} Faild 3 times, ${msg}"
-              fi
-              state=$(${REDIS_BIN_DIR}/redis-cli -h 127.0.0.1 -p ${port} info|awk  -F ':' '/role/ {print $NF}'|tr -d "\r")
-              ((++a))
-        done
-         log "Promoting redis-server to MASTER on ${port} SUCESS"
-         state=""
-   done
+         ${cmdsetmaster} >/dev/null ;
+     done
 }
 
 start_slave() {
@@ -60,7 +47,6 @@ start_slave() {
 	[ $? -eq 0 ]&&log "Promoting redis-server to Slave on ${port} SUCESS" ||log "Promoting redis-server to Slave on ${port} Faild"
      done
 }
-
 
 
 if [ $arg_m ]; then
